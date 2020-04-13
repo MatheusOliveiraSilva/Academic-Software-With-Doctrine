cfc1041638c02df93d421944f04298fafdadf0e8
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-13 08:47:29 -0300

    Create configurarion file for Doctrine ORM

---
+++ cli-config.php
@@ -0,0 +1,11 @@
+<?php
+
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+return ConsoleRunner::createHelperSet($entityManager);
