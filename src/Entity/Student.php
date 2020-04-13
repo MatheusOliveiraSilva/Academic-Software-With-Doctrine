ae97e6c1a7f8eaed4b1007a6f7b04fb03a213100
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-13 08:37:09 -0300

    Create Studant entity
    This class will manage and deal with students' data and provide all
    the business rules to persist them and manage them.

---
+++ src/Entity/Student.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Alura\Doctrine\Entity;
+
+/**
+ * @Entity
+ */
+
+class Student 
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Column(type="string")
+     */
+    private $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+   public function getName(): string
+   {
+       return $this->name;
+   }
+    
+    public function setName($name): self
+    {
+        $this->name = $name;
+	return $name;
+    }
+}
+
