5dd8eeec40cfd74b16477c65bcc7d6b00c38cdbc
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-16 12:12:13 -0300

    Create file to save courses into database
    Was created a file that can save courses into databases. It gets the
    argument passed throught the command line.

---
+++ commands/create-course.php
@@ -0,0 +1,15 @@
+<?php
+
+use Alura\Doctrine\Entity\Course;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once 'vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$course = new Course();
+$course->setName($argv[1]);
+
+$entityManager->persist($course);
+$entityManager->flush();
